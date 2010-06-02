ccaa5b1327f43207816ab61c5cf5f634f072489d
Finlay Cannon <fin@finlaycannon.com>
2010-06-02 00:33:51 -0700

    Added check to allow overwriting an existing symlink

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 function link() {
-    ln -sv $PWD/$1 ~/.$1
+    source=$PWD/$1
+    dotfile=~/.$1
+    
+    # if file exists and isn't a symlink, skip it 
+    if [ -e "${dotfile}" -a ! -h "${dotfile}" ]; then
+        echo "${dotfile} already exists"
+    else
+        ln -sfv ${source} ${dotfile}
+    fi
 }
 
 link bash_profile
